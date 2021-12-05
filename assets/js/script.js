d0dd91917afd5a097929a20b3ec3c7b572cf6f95
ЧакНорисс <64031009+Ility-47@users.noreply.github.com>
2021-12-05 18:48:27 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,54 @@ if(animItems.length > 0){
     }
     animOnScroll();
 }
+
+//хз как назвать
+let snow1 = document.getElementsByClassName('snow1').length;
+let snow2 = document.getElementsByClassName('snow2').length;
+let snow3 = document.getElementsByClassName('snow3').length;
+
+function randomInteger(min, max) {
+    
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+};
+
+var scrollHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
+
+function spawn(){
+    //first ball
+    for (i = snow1; i < 600; i++) {
+        const snowball1 = document.createElement('div');
+        snowball1.classList.add('snow1');
+        snowball1.style.left = randomInteger(5, window.innerWidth - 50) + 'px';
+        snowball1.style.top = randomInteger(50, scrollHeight) + 'px';
+
+        const snowball__animation1 = document.querySelector('.snow-anim');
+        snowball__animation1.appendChild(snowball1);
+    };
+    //double ball
+    for (i = snow2; i < 800; i++) {
+        const snowball2 = document.createElement('div');
+        snowball2.classList.add('snow2');
+        snowball2.style.left = randomInteger(5, window.innerWidth - 50) + 'px';
+        snowball2.style.top = randomInteger(50, scrollHeight) + 'px';
+
+        const snowball__animation2 = document.querySelector('.snow-anim');
+        snowball__animation2.appendChild(snowball2);
+    };
+    //thirst ball
+    for (i = snow3; i < 900; i++) {
+        const snowball3 = document.createElement('div');
+        snowball3.classList.add('snow3');
+        snowball3.style.left = randomInteger(5, window.innerWidth - 50) + 'px';
+        snowball3.style.top = randomInteger(50, scrollHeight) + 'px';
+
+        const snowball__animation3 = document.querySelector('.snow-anim');
+        snowball__animation3.appendChild(snowball3);
+    };
+};
+spawn();
